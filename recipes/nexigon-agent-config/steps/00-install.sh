58f2b9d8ab030d81f819eb3e7dc8d8db4f6a22ff
Maximilian Köhl <koehl@silitics.com>
2026-01-18 08:51:56 +0100

    feat: add `extra_config` parameter

--- recipes/nexigon-agent-config/steps/00-install.sh
+++ recipes/nexigon-agent-config/steps/00-install.sh
@@ -10,3 +10,5 @@ echo ".env" >> "${LAYER_REBUILD_IF_CHANGED}"
 . "${RUGIX_PROJECT_DIR}/.env"
 sed -i "s@%%HUB_URL%%@${NEXIGON_HUB_URL}@g" /etc/nexigon/agent.toml
 sed -i "s@%%TOKEN%%@${NEXIGON_TOKEN}@g" /etc/nexigon/agent.toml
+
+echo "${RECIPE_PARAM_EXTRA_CONFIG}" >> /etc/nexigon/agent.toml
